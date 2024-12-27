6989d6fe4bd5235c52334940567f7f0895a841e7
pc <sakurovadaria@gmail.com>
2024-12-27 16:51:10 +0500

    First commit

---
+++ src/main/java/com/spring/and/mockito/SpringAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.and.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMockitoApplication.class, args);
+	}
+
+}
